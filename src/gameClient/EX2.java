001b20ebfc433a77180e87fb62c5b37fdb6b77b2
eden <edensh9209@gmail.com>
2020-12-19 19:57:34 +0200

    change cmd

--- src/gameClient/EX2.java
+++ src/gameClient/EX2.java
@@ -20,18 +20,19 @@ public class EX2 {
 	static myGui mgg;
 
 	public static void main(String[] a) {
-		
-		new Management(); 
-		
+						
 		try {
 		if(a[0]!= null && a[1]!=null)
 		{
 			int id=Integer.parseInt(a[0]);
 			int s=Integer.parseInt(a[1]);
 			mgg = new myGui();
+			//public Automat(int s, game_service game, myGui mgg, int id) {
+
 			new Automat(s,null,mgg, id);
 
 		}
+		
 		
 		}
 		
@@ -39,6 +40,9 @@ public class EX2 {
 		{
 			System.out.println("you are dont use cmd command ");
 		}
+		
+		new Management(); 
+		
 	}
 }
 
